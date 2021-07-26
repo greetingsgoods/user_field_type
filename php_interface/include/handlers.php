f1e97a58cd95648489778f9fbad98e6696cd8e39
greetingsgoods <blololob078@gmail.com>
2021-07-26 14:28:59 +0300

    В созданном классе определим свой метод GetUserTypeDescription, собственный тип поля назовем c_local

--- php_interface/include/handlers.php
+++ php_interface/include/handlers.php
@@ -1,1 +1,25 @@
 <?php
+
+require __DIR__ . "/lang/" . LANGUAGE_ID . "/handlers.php";
+
+/*
+ * обработчик для класса MyCurledType
+ * - собственный тип пользовательского поля
+ * "Привязка к элементам инф. блоков с сортировкой"
+ */
+
+
+class MyCurledType extends CUserTypeIBlockElement
+{
+	// инициализация пользовательского свойства для главного модуля
+	public function GetUserTypeDescription()
+	{
+		return array(
+			"USER_TYPE_ID" => "c_local",
+			"CLASS_NAME" => "MyCurledType",
+			"DESCRIPTION" => GetMessage("USER_TYPE_IBEL_SORT_DESCRIPTION"),
+			"BASE_TYPE" => "int",
+		);
+	}
+
+}
